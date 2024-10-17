fe70358b96830cda1a728e61dced34d605a3d2eb
IZebaZI <zebaz.rg@outlook.com>
2024-10-16 18:20:52 -0600

    Repaso (Paso 3)
    Vista de Portada

--- practicasRepaso/resources/views/welcome.blade.php
+++ practicasRepaso/resources/views/welcome.blade.php
@@ -7,6 +7,21 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hola Mundo</h1>
+    <div class="container mt-3 text-center">
+        <div class="row gy-3">
+            <h1>REPASO LARAVEL</h1>
+            <h5>Escuela: Universidad Politécnica de Querétaro</h5>
+            <h5>Alumno: Ramírez García Sebastián</h5>
+            <h5>Carrera: Ingeniería en Sistemas Computacionales</h5>
+            <h5>Materia: Programación Web</h5>
+            <h5>Maestro: Ivan Isay Guerra López</h5>
+            <h5>Grupo: S195</h5>
+        </div>
+        <div class="row justify-content-center mt-3">
+            <div class="col-3">
+                <img class="img-fluid" src="{{asset('img/upq-logo.png')}}" alt="logo">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
